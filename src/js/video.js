1538c5843ff6bc64e60d5973eb6f0a6f95bde0af
JP <jpyepezimc@gmail.com>
2018-11-19 19:27:56 +1300

    trying metadata way

--- src/js/video.js
+++ src/js/video.js
@@ -3,7 +3,12 @@ const video = document.getElementById("videoElement");
 
 if(navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true})
-    .then(stream => video.srcObject = stream)
+    .then(stream => { 
+        video.srcObject = stream 
+        video.onloadedmetadata = function(e) {
+            video.play();
+        };
+    })
     .catch(err => console.error(err));
 }
 
