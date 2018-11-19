15b5489f3f0ab973e27d265f3454305291a94687
JP <jpyepezimc@gmail.com>
2018-11-19 20:14:45 +1300

    added webkit stuff

--- src/js/video.js
+++ src/js/video.js
@@ -1,14 +1,28 @@
 
 const video = document.getElementById("videoElement");
 
+if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function (resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+    }
+}
+
 if(navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true})
-    .then(stream => { 
-        video.srcObject = stream 
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
-    })
+    .then(stream => video.srcObject = stream)
     .catch(err => console.error(err));
 }
 
